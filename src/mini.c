13f7577f86ef8fe362a1f1e544f9fd10ae78b51b
R.A <rashedazizi975@gmail.com>
2025-12-10 19:17:26 +0900

    Add additional output message to mini CLI tool

--- src/mini.c
+++ src/mini.c
@@ -3,5 +3,6 @@
 int main(void) {
     printf("Mini CLI Tool is running!\n");
     printf("This program was built in Linux and packaged with Docker.\n");
+    printf("Everything works perfectly. lets'go!\n");
     return 0;
 }
